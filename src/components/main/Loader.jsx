92c08252abb059e416005bd421a4853ec0dbd3bd
Vladyslav Siminko <siminko.vlad23@gmail.com>
2021-06-23 19:46:38 +0300

    add loader component and styles

---
+++ src/components/main/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { LoaderStyled, DivStyled } from '@styles/styledComponents/styledLoader';
+
+export const Loader = () => (
+    <LoaderStyled>
+        <DivStyled />
+    </LoaderStyled>
+);
